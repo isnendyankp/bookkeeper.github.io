314673779f190b2f431e453687333fd82310e6e1
isnendyan <isnendyankp@gmail.com>
2023-04-17 09:13:13 +0700

    Building The Bookmarks: add link & append to bookmarks container

--- script.js
+++ script.js
@@ -55,6 +55,15 @@ function buildBookmarks() {
         const favicon = document.createElement('img');
         favicon.setAttribute('src', `https://s2.googleusercontent.com/s2/favicons?domain=${url}`);
         favicon.setAttribute('alt', 'Favicon');
+        // Link
+        const link = document.createElement('a');
+        link.setAttribute('href', `${url}`);
+        link.setAttribute('target', '_blank');
+        link.textContent = name;
+        // Append to bookmarks container
+        linkInfo.append(favicon, link);
+        item.append(closeIcon, linkInfo);
+        bookmarksContainer.appendChild(item);
     });
 }
 
